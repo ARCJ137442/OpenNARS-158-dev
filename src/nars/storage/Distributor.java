a7c2f380bc56a929084aa4281a8b3690b787e321
ARCJ137442 <61109168+ARCJ137442@users.noreply.github.com>
2024-06-09 23:33:12 +0800

    perf: :zap: 完全注释并优化「分派器」类型
    为「分派器」的「伪随机数生成算法」编写逐行注释，并引入next数组缓存以避免重复求模（缓存，以空间换时间）

--- src/nars/storage/Distributor.java
+++ src/nars/storage/Distributor.java
@@ -1,14 +1,18 @@
 package nars.storage;
 
+import java.util.Arrays;
+
 /**
  * A pseudo-random number generator, used in Bag.
  */
 class Distributor {
 
     /** Shuffled sequence of index numbers */
-    private final int order[];
+    private final int[] order;
     /** Capacity of the array */
     private final int capacity;
+    /** 🆕Cached "next" for order */
+    private final int[] next;
 
     /**
      * For any number N < range, there is N+1 copies of it in the array, distributed
@@ -17,22 +21,67 @@ class Distributor {
      * @param range Range of valid numbers
      */
     public Distributor(int range) {
-        // TODO: 过程笔记注释
-        int index, rank, time;
-        capacity = (range * (range + 1)) / 2;
-        order = new int[capacity];
-        for (index = 0; index < capacity; index++) {
-            order[index] = -1;
-        }
-        for (rank = range; rank > 0; rank--) {
-            for (time = 0; time < rank; time++) {
-                index = ((capacity / rank) + index) % capacity;
+        // * 🚩计算并分布列
+        this.order = createOrderFromRange(range);
+        // * 🚩计算总容量（就是「分布列」的长度）
+        this.capacity = order.length;
+        // * 🚩预缓存「下一索引」数组
+        this.next = createNextFromCapacity(this.capacity);
+    }
+
+    /**
+     * 🆕根据「范围」计算「分布列」
+     *
+     * @param range
+     * @param capacity
+     * @return
+     */
+    private static final int[] createOrderFromRange(final int range) {
+        // * 🚩计算总容量（就是「分布列」的长度）
+        final int capacity = (range * (range + 1)) / 2;
+        // * 🚩初始化分布列
+        final int[] order = new int[capacity];
+        // * 🚩全部初始化为-1（后续要用到）
+        Arrays.fill(order, -1);
+        // * 🚩正式开始计算
+        // * 📌【2024-06-09 23:11:27】约简index的初值 capacity -> 0 | 实际上「偏移取余」后跟0无区别
+        int index = 0;
+        // * 🚩在range~1（两端含）遍历
+        for (int rank = range; rank > 0; rank--) {
+            // * 🚩索引跳转的基位移 | 📝用于下方线性同余
+            final int baseOffset = capacity / rank;
+            // * 🚩重复rank次数
+            for (int time = 0; time < rank; time++) {
+                // * 🚩跳转索引 | 📝线性同余
+                index += baseOffset;
+                index %= capacity;
+                // * 🚩向后找到第一个未初始化的地方
                 while (order[index] >= 0) {
-                    index = (index + 1) % capacity;
+                    index += 1;
+                    index %= capacity;
                 }
+                // * 🚩设置值
                 order[index] = rank - 1;
             }
         }
+        // * 🚩返回
+        return order;
+    }
+
+    /**
+     * 🆕根据已有「容量」创建「下一索引」数组
+     *
+     * @param capacity 容量
+     * @return 「下一索引」数组
+     */
+    private static final int[] createNextFromCapacity(final int capacity) {
+        // * 🚩创建新数组
+        final int[] next = new int[capacity];
+        for (int index = 0; index < capacity; index++) {
+            next[index] = index + 1;
+        }
+        next[capacity - 1] = 0;
+        return next;
     }
 
     /**
@@ -42,7 +91,7 @@ class Distributor {
      * @return the random value
      */
     public int pick(int index) {
-        return order[index];
+        return this.order[index];
     }
 
     /**
@@ -52,6 +101,6 @@ class Distributor {
      * @return the next index
      */
     public int next(int index) {
-        return (index + 1) % capacity;
+        return this.next[index];
     }
 }
